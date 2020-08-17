e016a5eeb41ce3e60fc52cecfcbfc1120280a8bd
RoyalCookieX <40614905+RoyalCookieX@users.noreply.github.com>
2020-08-16 20:52:20 -0500

    Aug. 16, 2020 #2
    -Updated with player movement and camera movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class Player : Character
 {
-    [SerializeField] Camera cam; //Temporary
+    Camera cam;
+
+    [Header("Player Properties")]
     [SerializeField] Rect healthRect;
     [SerializeField] Rect ammoRect;
     [SerializeField] Rect cooldownRect;
     [SerializeField] Gradient gradient;
 
+    [Space]
+    [Header("Player Properties")]
+    //From Playermov.cs
+    //[SerializeField] float playerSpeedHorizontal = 10;
+    //[SerializeField] float playerSpeedVertical = 10;
+    [SerializeField] float playerSpeed = 5;
+    [SerializeField] float lerpSpeed = 2;
+    float smoothSpeed;
+
     private void Start()
     {
-        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        cam = GameCamera.Instance.Cam;
     }
 
     void Update()
     {
-        Vector2 armCenter = cam.WorldToScreenPoint(arm.transform.position);
-        float angle = Vector2.SignedAngle(Vector2.right, (Vector2)Input.mousePosition - armCenter);
-        SetArmAngle(angle);
+        if(cam)
+        {
+            Vector2 armCenter = cam.WorldToScreenPoint(arm.transform.position);
+            float angle = Vector2.SignedAngle(Vector2.right, (Vector2)Input.mousePosition - armCenter);
+            SetArmAngle(angle);
+        }
 
         if (weapon)
         {
@@ -28,6 +42,15 @@ public class Player : Character
             if (Input.GetButtonDown("Fire2")) weapon.Reload();
             if (Input.GetKeyDown(KeyCode.Space)) DropWeapon();
         }
+
+        //From Playermov.cs
+        //float transV = Input.GetAxis("Vertical") * playerSpeedVertical * Time.deltaTime;
+        //float transH = Input.GetAxis("Horizontal") * playerSpeedHorizontal * Time.deltaTime;
+
+        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        smoothSpeed = Mathf.Lerp(smoothSpeed, dir.magnitude > 0 ? playerSpeed : 0, Time.deltaTime * lerpSpeed);
+
+        transform.Translate(dir * smoothSpeed * Time.deltaTime);
     }
 
     private void OnGUI()
