6b7def3f3bbd5335fbee56613ebfcb97c2e06219
RoyalCookieX <royalcookiex@gmail.com>
2020-08-20 10:36:09 -0500

    Aug. 20, 2020 #1
    -Added Reload Icon
    -Edited Background
    -Reduced Player Speed

--- Assets/Scripts/GUI/PlayerGUI.cs
+++ Assets/Scripts/GUI/PlayerGUI.cs
@@ -11,19 +11,30 @@ public class PlayerGUI : MonoBehaviour
     [Header("PlayerGUI Components")]
     [SerializeField] Transform healthbar;
     [SerializeField] TMP_Text ammoText;
+    [SerializeField] Transform reloadIcon;
+
+    [Space]
+    [Header("PlayerGUI Properties")]
+    [SerializeField] float reloadIconSpeed = -120;
 
     private void Update()
     {
         if (!Player) return;
-        healthbar.localScale = new Vector3(Player.Health / 100, 1, 1);
+        healthbar.localScale = new Vector3(Mathf.Clamp(Player.Health / 100, 0, 1), 1, 1);
 
         if (Player.Weapon)
         {
             ammoText.text = $"{Player.Weapon.CurAmmo}/{Player.Weapon.GetWeaponData().ammo}";
+            reloadIcon.gameObject.SetActive(Player.Weapon.CurCooldown > 0);
+            if (Player.Weapon.CurCooldown > 0)
+            {
+                reloadIcon.transform.rotation = Quaternion.AngleAxis(Time.time * reloadIconSpeed, Vector3.forward);
+            }
         }
         else
         {
             ammoText.text = "";
+            reloadIcon.gameObject.SetActive(false);
         }
     }
 }
